c5d6548452c33e958dc59b9ada254a2214d6a073
Donovan Jenkins <djenkins@icasa.nmt.edu>
2023-05-25 17:52:07 -0600

    Thingy now does a cool fade out thingy when you click on it.

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom'
 import logo from './assets/logo.svg'
 import "./Portal.css"
-import React from 'react';
+import React, { useState } from 'react';
 
+import { animated, useSpring } from '@react-spring/web'
+
+// TODO: display who is signed to the label as bubbles when they click on the logo
 function Portal() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const styles = useSpring({
+    opacity: isVisible ? 1 : .1,
+    y: isVisible ? 0 : 24,
+  })
+
   return (
-    <>
-      <Link to="/landing">
-        <img src={logo} className="logo" alt="React logo" />
-      </Link>
-      <h1>Space Hash Records</h1>
-      <p className="subtitle">
-        Pushing boundaries since 2023
-      </p>
-    </>
+    <div>
+      <animated.div style={styles}>
+        <img src={logo} className="logo" alt="React logo" style={{ cursor: "grab" }} onClick={() => setIsVisible(!isVisible)}/>
+        <h1>Space Hash Records</h1>
+        <p className="subtitle">Pushing boundaries since 2023</p>
+      </animated.div>
+    </div>
   )
 }
 
